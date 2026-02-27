5b0ebaa39531362e6ef1bb44bb19283bf8b488d7
Felix Hua <felix.hua@student.tdsb.on.ca>
2026-02-27 09:46:23 -0500

    Added triple root solution

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 const cubic = document.getElementById("cubic") as HTMLFormElement;
 
 const fixDecimal = (value: number): number => {
-    return parseFloat(String(value.toFixed(12)));
+    return parseFloat(value.toFixed(12));
 }
 
-const trigSolve = (a: number, b: number, p: number, q: number): number[] => {
+const trigSolve = (a: number, b: number, p: number, q: number): [number, number, number] => {
     const theta: number = Math.acos(-q / (2 * Math.sqrt(-((p / 3) ** 3)))) / 3;
     const x1: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta) - b / (3 * a);
     const x2: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta + 2 * Math.PI / 3) - b / (3 * a);
@@ -27,11 +27,17 @@ cubic.addEventListener("submit", (event) => {
     const p: number = (3 * a * c - b ** 2) / (3 * a ** 2);
     const q: number = (27 * a ** 2 * d - 9 * a * b * c + 2 * b ** 3) / (27 * a ** 3);
     const discriminant = Number(fixDecimal((q / 2) ** 2 + (p / 3) ** 3));
+    let solutions: [number, number | string, number | string] = [0, 0, 0]
     if (discriminant < 0) {
-        const solutions: number[] = trigSolve(a, b, p, q);
-        console.log(solutions)
+        solutions = trigSolve(a, b, p, q);
     } else if (discriminant > 0) {
-        const solutions: [number, string, string] = [cardano(a, b, q, discriminant), "Complex", "Complex"];
-        console.log(solutions)
+        solutions = [cardano(a, b, q, discriminant), "Complex", "Complex"];
+    } else {
+        if (p == 0 && q == 0) {
+            for (let i = 0; i < 3; i++) {
+                solutions[i] = cardano(a, b, q, discriminant);
+            }
+        }
     }
+    console.log(solutions)
 })
