f9ba22a3cb027f8bf4073dab0c402acdd82a8a83
Felix Hua <felix.hua@student.tdsb.on.ca>
2026-03-04 10:18:40 -0500

    Started graph and reformatted table

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-const cubic = document.getElementById("cubic") as HTMLFormElement;
+const cubic: HTMLFormElement = document.getElementById("cubic") as HTMLFormElement;
+const graph: HTMLCanvasElement = document.getElementById("graph") as HTMLCanvasElement;
+const ctx: CanvasRenderingContext2D = graph.getContext("2d") as CanvasRenderingContext2D;
 
-const fixDecimal = (value: number): number => {
-    return parseFloat(value.toFixed(12));
+const fixDecimal = (value: number, fix: number): number => {
+    return parseFloat(value.toFixed(fix));
 }
 
 const trigSolve = (a: number, b: number, p: number, q: number): [number, number, number] => {
@@ -9,12 +11,12 @@ const trigSolve = (a: number, b: number, p: number, q: number): [number, number,
     const x1: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta) - b / (3 * a);
     const x2: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta + 2 * Math.PI / 3) - b / (3 * a);
     const x3: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta + 4 * Math.PI / 3) - b / (3 * a);
-    return [fixDecimal(x1), fixDecimal(x2), fixDecimal(x3)];
+    return [fixDecimal(x1, 2), fixDecimal(x2, 2), fixDecimal(x3, 2)];
 }
 
 const cardano = (a: number, b: number, q: number, discriminant: number): number => {
     const x: number = Math.cbrt(-q / 2 + discriminant) + Math.cbrt(-q / 2 - discriminant) - b / (3 * a);
-    return x;
+    return fixDecimal(x, 2);
 }
 
 cubic.addEventListener("submit", (event) => {
@@ -27,7 +29,7 @@ cubic.addEventListener("submit", (event) => {
 
     const p: number = (3 * a * c - b ** 2) / (3 * a ** 2);
     const q: number = (27 * a ** 2 * d - 9 * a * b * c + 2 * b ** 3) / (27 * a ** 3);
-    const discriminant = Number(fixDecimal((q / 2) ** 2 + (p / 3) ** 3));
+    const discriminant = Number(fixDecimal((q / 2) ** 2 + (p / 3) ** 3, 12));
 
     let solutions: [number, number | string, number | string] = [0, 0, 0]
     if (discriminant < 0) {
@@ -53,16 +55,43 @@ cubic.addEventListener("submit", (event) => {
     const tableValues = document.getElementsByClassName("table-value") as HTMLCollectionOf<HTMLElement>;
 
     equation.style.display = "block";
-    tableValues[0].innerText = String(p)
-    tableValues[1].innerText = String(q)
-    tableValues[2].innerText = String(discriminant)
+    tableValues[0].innerText = String(p);
+    tableValues[1].innerText = String(q);
+    tableValues[2].innerText = String(discriminant);
+    ctx.clearRect(0, 0, 600, 600)
+    ctx.beginPath();
+    for (let i = 0; i < 30; i++) {
+        ctx.moveTo(i * 20, 0);
+        ctx.lineTo(i * 20, 600);
+        ctx.moveTo(0, i * 20);
+        ctx.lineTo(600, i * 20);
+    }
+    ctx.strokeStyle = "rgb(200, 200, 200)"
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(300, 0);
+    ctx.lineTo(300, 600);
+    ctx.moveTo(0, 300);
+    ctx.lineTo(600, 300);
+    ctx.strokeStyle = "black"
+    ctx.stroke();
+    ctx.beginPath();
     for (let i = 0; i < 3; i++) {
-        tableValues[i+3].innerText = String(solutions[i])
-        if (coefficients[i] == 1) {
-            coefficientDisplay[i].innerText = ""
+        if (typeof tableValues[i + 3] === "number") {
+            tableValues[i + 3].innerText = `(${String(solutions[i])}, 0)`;
         } else {
-            coefficientDisplay[i].innerText = String(coefficients[i])
+            tableValues[i + 3].innerText = String(solutions[i]);
+        }
+        if (coefficients[i] == 1) {
+            coefficientDisplay[i].innerText = "";
+        } else {
+            coefficientDisplay[i].innerText = String(coefficients[i]);
+        }
+        if (typeof solutions[i] === "number") {
+            ctx.arc(Number(solutions[i]) * 20 + 300, 300, 3, 0, 2 * Math.PI);
         }
     }
-    coefficientDisplay[3].innerText = String(d)
+    coefficientDisplay[3].innerText = String(d);
+    ctx.fillStyle = "blue";
+    ctx.fill();
 })
